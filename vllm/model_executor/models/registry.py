77a6cf1cab1b95b00a3dab13283725026294699c
Dong Wang <dongw2019@gmail.com>
2026-04-28 01:19:13 +0000

    intial commit based on exsiting parakeet decoder for nanotron model
    Signed-off-by: Dong Wang <dongw2019@gmail.com>

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -586,6 +586,7 @@ _MULTIMODAL_MODELS = {
         "nemotron_parse",
         "NemotronParseForConditionalGeneration",
     ),
+    "ParakeetForTDT": ("parakeet_tdt", "ParakeetForTDT"),
     "WhisperForConditionalGeneration": ("whisper", "WhisperForConditionalGeneration"),
 }
 
